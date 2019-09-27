c99db415927075b7f311dd77a793e446fb9f76d0
Cody Chan <int64ago@gmail.com>
2019-09-27 12:23:32 +0800

    feat: set engine version from current svrx

--- template/example/start.js
+++ template/example/start.js
@@ -1,19 +1,17 @@
 const path = require('path');
-const { existsSync } = require('fs');
+const reguireg = require('requireg');
 
-const svrxCLI = path.resolve(require('global-modules'), '@svrx', 'cli', 'lib');
-
-if (!existsSync(svrxCLI)) {
+let Manager;
+try {
+  Manager = reguireg('@svrx/cli/lib');
+} catch (err) {
   console.log('Please install svrx-cli by `npm i @svrx/cli -g`');
   process.exit();
 }
 
-const Manager = require(svrxCLI);
-
 process.chdir(__dirname);
 
-const manager = new Manager();
-manager.loadConfigFile();
+new Manager(); // eslint-disable-line
 Manager.loadSvrx({}, {
   root: __dirname,
   plugins: [{ path: path.resolve('..') }],
